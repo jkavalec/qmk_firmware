e0765eccb54f3e75f1ced6ea2bd60a7370b686ad
Jarek <jaroslav.kavalec@gmail.com>
2023-02-26 14:56:01 +0100

    ergodox rules change

--- keyboards/ergodox_ez/rules.mk
+++ keyboards/ergodox_ez/rules.mk
@@ -14,13 +14,16 @@ BOOTLOADER = halfkay
 #
 BOOTMAGIC_ENABLE = no   # Enable Bootmagic Lite
 MOUSEKEY_ENABLE  = yes  # Mouse keys
-EXTRAKEY_ENABLE  = yes  # Audio control and System control
-CONSOLE_ENABLE   = no   # Console for debug
+EXTRAKEY_ENABLE  = no  # Audio control and System control
+CONSOLE_ENABLE   = yes   # Console for debug
 COMMAND_ENABLE   = no   # Commands for debug and configuration
+DEBUG_ENABLE   = yes   # Commands for debug and configuration
 CUSTOM_MATRIX    = lite # Custom matrix file for the ErgoDox EZ
-NKRO_ENABLE      = yes           # Enable N-Key Rollover
-UNICODE_ENABLE   = no  # Unicode
+NKRO_ENABLE      = no           # Enable N-Key Rollover
+UNICODEMAP_ENABLE   = yes  # Unicode
 SWAP_HANDS_ENABLE= no   # Allow swapping hands of keyboard
+SEND_STRING_ENABLE = yes
+
 
 RGB_MATRIX_ENABLE = no # enable later
 RGB_MATRIX_DRIVER = IS31FL3731
