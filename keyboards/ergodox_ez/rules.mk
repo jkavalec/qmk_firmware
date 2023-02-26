1d002ae67ae7801a7ee5ce611c66a112bf45388e
Jarek <jaroslav.kavalec@gmail.com>
2023-02-26 15:04:41 +0100

    test

--- keyboards/ergodox_ez/rules.mk
+++ keyboards/ergodox_ez/rules.mk
@@ -7,7 +7,7 @@ BOOTLOADER = halfkay
 # If you have Left LEDs (see
 # https://geekhack.org/index.php?topic=22780.msg873819#msg873819 for
 # details), include the following define:
-# OPT_DEFS += -DLEFT_LEDS
+
 
 # Build Options
 #   change yes to no to disable
